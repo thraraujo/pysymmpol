b0503bb3091b59b40c0ec3888819c61badb534b5
thraraujo <thgr.araujo@gmail.com>
2024-03-16 07:41:11 -0400

    start poetry project

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from pysymmpol.young import YoungDiagram
+from pysymmpol.conjugacy import ConjugacyClass
+from pysymmpol.states import State
+from pysymmpol.homogeneous import HomogeneousPolynomial
+from pysymmpol.elementary import ElementaryPolynomial
+from pysymmpol.schur import SchurPolynomial
+from pysymmpol.hall import HallLittlewoodPolynomial, MonomialPolynomial
